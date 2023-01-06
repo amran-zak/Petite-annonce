ddc72f3581e904ab2d8a66493eed5e2db59360bb
perrineosw <oswald.perrinedu40@gmail.com>
2023-01-06 16:30:59 +0100

    login / sign up / new mdp

--- petite-annonce-frontend/src/Rooting/AppRouter.tsx
+++ petite-annonce-frontend/src/Rooting/AppRouter.tsx
@@ -3,6 +3,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NotFoundPage from "../Components/404/NotFound";
 import Header from "../Components/NavBar/Header";
 import Home from "../Components/Annonces/Home";
+import Login from "../Components/Auth/Login";
+import SignUp from "../Components/Auth/SignUp";
+import New_password from "../Components/Auth/New_password";
+
 
 export default function AppRouter(): JSX.Element {
   return (
@@ -13,8 +17,10 @@ export default function AppRouter(): JSX.Element {
         </div>
         <div className="app-body">
           <Routes>
-            <Route path="/login" element="" />
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Home/>} />
+            <Route path="/connexion" element={<Login/>} />
+            <Route path="/inscription" element={<SignUp/>} />
+            <Route path="/nouveau_mot_de_passe" element={<New_password/>} />
             {/*<Route path='/forbiddenAccess' element={<LoginPage/>} />*/}
             <Route path="/*" element={<NotFoundPage />} />
           </Routes>
