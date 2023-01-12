bfc0d0a7ff5cba2c601b26d37f94e20ef8f4e54b
Amran_zak <80887236+amran-zak@users.noreply.github.com>
2023-01-12 12:56:09 +0100

    deploiement : delete connexionn of db

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,7 @@ import { MongooseModule} from "@nestjs/mongoose";
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [
-      MongooseModule.forRoot(
-          "mongodb+srv://<rootAccess>:<UfsREETshSn9igN>@cluster0.kngtf.mongodb.net/session-auth?retryWrites=true&w=majority"
-      ),
-      UsersModule,
-  ],
+  
   controllers: [AppController],
   providers: [AppService],
 })
