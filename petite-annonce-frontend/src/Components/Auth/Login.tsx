c0d26056ea00926860636fae6f208df52ff4df48
perrineosw <oswald.perrinedu40@gmail.com>
2023-01-12 10:09:20 +0100

    modification des pages, possibilité d'ajouter une image lors de l'inscription

--- petite-annonce-frontend/src/Components/Auth/Login.tsx
+++ petite-annonce-frontend/src/Components/Auth/Login.tsx
@@ -23,89 +23,89 @@ export default function Login(): JSX.Element {
     };
     return (
         <ThemeProvider theme={theme}>
-        <Grid container component="main" sx={{ height: '100vh' }}>
-            <CssBaseline />
-            <Grid
-            item
-            xs={false}
-            sm={4}
-            md={7}
-            sx={{
-                backgroundImage: `url(${background})`,
-                backgroundRepeat: 'no-repeat',
-                backgroundColor: (t) =>
-                t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-            }}
-            />
-            <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-            <Box
+            <Grid container component="main" sx={{ height: '100vh' }}>
+                <CssBaseline/>
+                <Grid
+                item
+                xs={false}
+                sm={4}
+                md={7}
                 sx={{
-                my: 8,
-                mx: 4,
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
+                    backgroundImage: `url(${background})`,
+                    backgroundRepeat: 'no-repeat',
+                    backgroundColor: (t) =>
+                    t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
                 }}
-            >
-                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                <LockOutlinedIcon />
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                Connexion
-                </Typography>
-                <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
-                <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="email"
-                    label="Adresse Email"
-                    name="email"
-                    autoComplete="email"
-                    autoFocus
                 />
-                <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    name="password"
-                    label="Mot de passe"
-                    type="password"
-                    id="password"
-                    autoComplete="current-password"
-                />
-                <FormControlLabel
-                    control={<Checkbox value="remember" color="primary" />}
-                    label="Rester connecté"
-                />
-                <Button
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    sx={{ mt: 3, mb: 2 }}
-                >
-                    Connexion
-                </Button>
-                <Grid container sx={{ mt: 2}}>
-                    <Grid item>
-                    <Link href="/nouveau_mot_de_passe" variant="body2">
-                        Vous avez oublié votre mot de passe ?
-                    </Link>
-                    </Grid>
+                <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                    <Box
+                        sx={{
+                        my: 8,
+                        mx: 4,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        }}
+                    >
+                        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                            <LockOutlinedIcon/>
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Connexion
+                        </Typography>
+                        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Adresse Email"
+                                name="email"
+                                autoComplete="email"
+                                autoFocus
+                            />
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Mot de passe"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                            />
+                            <FormControlLabel
+                                control={<Checkbox value="remember" color="primary" />}
+                                label="Rester connecté"
+                            />
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2 }}
+                            >
+                                Connexion
+                            </Button>
+                            <Grid container sx={{ mt: 2}}>
+                                <Grid item>
+                                <Link href="/nouveau_mot_de_passe" variant="body2">
+                                    Vous avez oublié votre mot de passe ?
+                                </Link>
+                                </Grid>
+                            </Grid>
+                            <Grid container sx={{ mt: 2}}>
+                                <Grid item>
+                                <Link href="/inscription" variant="body2">
+                                    {"Vous n'avez pas de compte ? Inscrivez-vous !"}
+                                </Link>
+                                </Grid>
+                            </Grid>
+                        </Box>
+                    </Box>
                 </Grid>
-                <Grid container sx={{ mt: 2}}>
-                    <Grid item>
-                    <Link href="/inscription" variant="body2">
-                        {"Vous n'avez pas de compte ? Inscrivez-vous !"}
-                    </Link>
-                    </Grid>
-                </Grid>
-                </Box>
-            </Box>
             </Grid>
-        </Grid>
         </ThemeProvider>
     );
 }
